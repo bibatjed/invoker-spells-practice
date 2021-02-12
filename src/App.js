91eb92e895ed470f248bffa1ef0ab0511778dab4
Jed <bibatjed@gmail.com>
2021-02-12 13:09:21 +0800

    feat: container background and title, add basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import SkillsSection from './SkillsSection/SkillsSection'
+import SkillsSection from "./SkillsSection/SkillsSection";
+
+//img
+import InvokerHero from "./images/floating_invoker.png";
 
 function App() {
   return (
     <div className="App">
-      <SkillsSection/>
+      <div className="Container">
+        <div className="Container__Title">
+          <h1>Invoker Skills</h1>
+          <div className="Container__Title--Image">
+            <img src={InvokerHero}></img>
+          </div>
+        </div>
+        <SkillsSection/>
+      </div>
+
     </div>
   );
 }
