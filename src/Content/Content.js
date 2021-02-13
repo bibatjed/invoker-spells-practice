28ae2c259b75552822c1d013cc6e1c40ad7d2c73
Jed <bibatjed@gmail.com>
2021-02-13 13:11:40 +0800

    feat: Content 'Press Enter to start the game' (WIP)

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -12,6 +12,7 @@ import ExortIcon from "../images/exort_icon.png";
 import BlankIcon from "../images/dota2_icon.jpg";
 
 //CSS
+import "./Content.css";
 
 const CheckIconBaseOnKey = (key) => {
   switch (key) {
@@ -33,6 +34,8 @@ function Content() {
     BlankIcon,
   ]);
 
+  const [isGameStarted, setGameStarted] = useState(false);
+
   useKey(["KeyQ", "KeyW", "KeyE"], (event) => {
     setCurrentSKillSet((currentSkillSetArray) => [
       ...currentSkillSetArray.slice(1),
@@ -40,13 +43,25 @@ function Content() {
     ]);
   });
 
-  console.log(currentSkillSet)
+  useKey(["Enter"], () => {
+    setGameStarted((currentGameStartedStatus) => !currentGameStartedStatus);
+  });
 
   return (
-    <div className="skillsSection">
-      <OrbInput image={currentSkillSet[0]} />
-      <OrbInput image={currentSkillSet[1]} />
-      <OrbInput image={currentSkillSet[2]} />
+    <div className="Content">
+      <div className="Instructions">
+        <div className="Instructions__Title">
+          <span>Instructions</span>
+        </div>
+        <div className="Instructions__SkillsQuest">
+        {!isGameStarted ? <span>Press Enter to Start the Game</span> : <img src={BlankIcon}></img>}
+        </div>
+      </div>
+      <div className="Content__Orb">
+        <OrbInput image={currentSkillSet[0]} />
+        <OrbInput image={currentSkillSet[1]} />
+        <OrbInput image={currentSkillSet[2]} />
+      </div>
     </div>
   );
 }
